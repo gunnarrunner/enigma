cb871d978ea3ffd9d3f7cbba1526423a70b93ea3
Gunnar <dunkcrunk.com>
2021-04-25 21:58:13 -0600

    Made tests work for all of my encrypt and decrypt methods.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,17 @@
+require 'date'
+
+
 class Enigma
 ALPHABET = ("a".."z").to_a << " "
 
-  attr_reader :key_encrypt_arrays,
+  attr_accessor :key_encrypt_arrays,
               :key_decrypt_arrays
   def initialize
       @key_encrypt_arrays = []
       @key_decrypt_arrays = []
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key = false, date = false)
     last_4_of_date(date)
     key_encrypt_values(key)
     combined_shift = last_4_of_date(date).zip(@key_encrypt_arrays)
@@ -27,42 +30,67 @@ ALPHABET = ("a".."z").to_a << " "
     encrypt
   end
 
-  def decrypt(code, key, date)
+  def decrypt(code, key = false, date = false)
     last_4_of_date(date)
     key_decrypt_values(key)
     combined_shift = last_4_of_date(date).zip(@key_decrypt_arrays)
     shift = combined_shift.map do |shift|
       shift.sum
     end
-    x = message.chars.map {|c| ALPHABET.index(c)}
-    y = x.zip(shift.cycle(4)).map(&:sum)
+    negative_shift = shift.map {|shift| shift * -1}
+    x = code.chars.map {|c| ALPHABET.index(c)}
+    y = x.zip(negative_shift.cycle(4)).map(&:sum)
     z = y.map {|c| ALPHABET[c % 27]} 
     a = z.join("")
 
     encrypt = {}
-    encrypt["encryption".to_sym] = a
+    encrypt["decryption".to_sym] = a
     encrypt["key".to_sym] = key
     encrypt["date".to_sym] = date
     encrypt
   end
 
   def last_4_of_date(date)
-    squared = date.to_i**2
-    last_4_string = squared.to_s.chars.last(4)
-    last_4_string.map do |integer|
-      integer.to_i
+    if date == false
+      date_now = Time.now
+      date = date_now.strftime("%d%m%y")
+      squared = date.to_i**2
+      last_4_string = squared.to_s.chars.last(4)
+      last_4_string.map do |integer|
+        integer.to_i
+      end
+    else
+      squared = date.to_i**2
+      last_4_string = squared.to_s.chars.last(4)
+      last_4_string.map do |integer|
+        integer.to_i
+      end
     end
   end
 
   def key_encrypt_values(key)
-    key.chars.each_cons(2) do |number_set|
+    if key == false
+      key = rand(10000..99999).to_s
+      key.chars.each_cons(2) do |number_set|
       @key_encrypt_arrays <<  number_set.join.to_i
+    end
+    else
+      key.chars.each_cons(2) do |number_set|
+      @key_encrypt_arrays <<  number_set.join.to_i
+      end
     end
   end
 
   def key_decrypt_values(key)
-    key.chars.each_cons(2) do |number_set|
-      @key_decrypt_arrays <<  number_set.join.to_i
+    if key == false
+      key = rand(10000..99999).to_s
+      key.chars.each_cons(2) do |number_set|
+        @key_decrypt_arrays <<  number_set.join.to_i
+      end
+    else
+      key.chars.each_cons(2) do |number_set|
+        @key_decrypt_arrays <<  number_set.join.to_i
+      end
     end
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -32,4 +32,47 @@ RSpec.describe Enigma do
 
     expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
   end
+
+  it 'can encrypt a message with a key and no date' do
+    enigma = Enigma.new
+
+    expected = {
+                encryption: "qgfax",
+                key: "02715",
+                date: false
+                }
+
+    expect(enigma.encrypt("hello", "02715")).to eq(expected)
+  end
+
+  it 'can decrypt a message with a key and not a date' do
+    enigma = Enigma.new
+
+    expected = {
+                decryption: "hello",
+                key: "02715",
+                date: false
+                }
+
+    expect(enigma.decrypt("qgfax", "02715")).to eq(expected)
+  end
+
+  it 'can encrypt a message without a key and a date' do
+    Enigma.any_instance.stub(:key_encrypt_values).and_return([1,2,3,4])
+    Enigma.any_instance.stub(:key_encrypt_arrays).and_return([5,6,7,8])
+
+    enigma = Enigma.new
+
+    expected = {
+                encryption: "tmwu " ,
+                key: false,
+                date: false
+                }
+
+      # Enigma.any_instance.stub(:key_encrypt_values).and_return([1,2,3,4])
+      # Enigma.any_instance.stub(:key_encrypt_arrays).and_return([5,6,7,8])
+      enigma.key_encrypt_arrays = [5,6,7,8]
+      
+      expect(enigma.encrypt("hello")).to eq(expected)
+  end
 end
